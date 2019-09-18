0600cbfef9ccab147987761072f7ee6d90f036ac
Scott Sievert <dev@stsievert.com>
2019-09-18 14:54:10 -0500

    Update version

--- altair_widgets/__init__.py
+++ altair_widgets/__init__.py
@@ -1,4 +1,4 @@
 # update with `python setup.py build; python setup.py sdist upload`
-__version__ = '0.2.0'
+__version__ = '0.2.2'
 
 from .widget import interact_with
--- setup.py
+++ setup.py
@@ -16,8 +16,7 @@ AUTHOR_EMAIL        = "dev@stsievert.com"
 URL                 = 'http://altair-viz.github.io'
 DOWNLOAD_URL        = 'http://github.com/altair-viz/altair_widgets/'
 LICENSE             = 'BSD 3-clause'
-with open('./requirements.txt', 'r') as f:
-    INSTALL_REQUIRES = [x for x in f.read().split('\n') if len(x) > 0]
+INSTALL_REQUIRES = ["ipython", "ipywidgets>=7.0.0", "altair>=2.0.0", "vega>=0.4.4"]
 
 import io
 import os
